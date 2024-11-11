bdc2a91fa9e02df5d8d1ec43b22168f890c6c565
nguyenvanlap1 <nguyenvanlap3965@gmail.com>
2024-11-11 21:30:22 +0700

    add s

--- citaa/src/main/java/com/citaa/citaa/config/WebsocketConfig.java
+++ citaa/src/main/java/com/citaa/citaa/config/WebsocketConfig.java
@@ -12,7 +12,7 @@ public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/wss").setAllowedOriginPatterns("*").withSockJS();
     }
 
     @Override
