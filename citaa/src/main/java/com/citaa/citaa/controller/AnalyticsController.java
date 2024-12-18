401a92a5ce40acc40611edc7933fab6a2dfa82a1
nhthi <nguyenhungthinh574@gmail.com>
2024-12-18 09:35:34 +0700

    up

--- citaa/src/main/java/com/citaa/citaa/controller/AnalyticsController.java
+++ citaa/src/main/java/com/citaa/citaa/controller/AnalyticsController.java
@@ -1,33 +1,33 @@
-package com.citaa.citaa.controller;
-
-import com.citaa.citaa.service.AnalyticsDataService;
-import com.citaa.citaa.service.GoogleAnalyticsFetcher;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
-
-@RestController
-@RequestMapping("/analytics")
-public class AnalyticsController {
-
-    @Autowired
-    private AnalyticsDataService analyticsDataService;
-
-    @GetMapping("/fetch")
-    public ResponseEntity<String> fetchData() {
-        try {
-            GoogleAnalyticsFetcher fetcher = new GoogleAnalyticsFetcher();
-            fetcher.fetchData();
-            return ResponseEntity.ok("Data fetched successfully!");
-        } catch (IOException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error fetching data");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-}
+//package com.citaa.citaa.controller;
+//
+//import com.citaa.citaa.service.AnalyticsDataService;
+//import com.citaa.citaa.service.GoogleAnalyticsFetcher;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.io.IOException;
+//
+//@RestController
+//@RequestMapping("/analytics")
+//public class AnalyticsController {
+//
+//    @Autowired
+//    private AnalyticsDataService analyticsDataService;
+//
+//    @GetMapping("/fetch")
+//    public ResponseEntity<String> fetchData() {
+//        try {
+//            GoogleAnalyticsFetcher fetcher = new GoogleAnalyticsFetcher();
+//            fetcher.fetchData();
+//            return ResponseEntity.ok("Data fetched successfully!");
+//        } catch (IOException e) {
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error fetching data");
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//}
--- citaa/src/main/java/com/citaa/citaa/service/AnalyticsDataService.java
+++ citaa/src/main/java/com/citaa/citaa/service/AnalyticsDataService.java
@@ -1,21 +1,21 @@
-package com.citaa.citaa.service;
-
-import com.citaa.citaa.model.AnalyticsData;
-import com.citaa.citaa.repository.AnalyticsDataRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
-public class AnalyticsDataService {
-
-    @Autowired
-    private AnalyticsDataRepository repository;
-
-    public void saveAnalyticsData(String metricName, Long value) {
-        AnalyticsData data = new AnalyticsData();
-        data.setMetricName(metricName);
-        data.setValue(value);
-        repository.save(data);
-    }
-}
-
+//package com.citaa.citaa.service;
+//
+//import com.citaa.citaa.model.AnalyticsData;
+//import com.citaa.citaa.repository.AnalyticsDataRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class AnalyticsDataService {
+//
+//    @Autowired
+//    private AnalyticsDataRepository repository;
+//
+//    public void saveAnalyticsData(String metricName, Long value) {
+//        AnalyticsData data = new AnalyticsData();
+//        data.setMetricName(metricName);
+//        data.setValue(value);
+//        repository.save(data);
+//    }
+//}
+//
